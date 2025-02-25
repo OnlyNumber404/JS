ff3b51559722303c1356ff8fb1241de6fb521c57
OnlyNumber404 <agustinusdenny110@gmail.com>
2025-02-25 22:31:44 +0700

    add Catch in Promise

--- Promiselatihan.js
+++ Promiselatihan.js
@@ -5,22 +5,13 @@ const order = 'Kopi Espresso';
 console.log(`Saya memesan ${order} di kafe.`);
 
 makeCoffee(order)
-  .then(
-    (value) => {
+  .then((value) => {
       return sendCoffee(value);
-    },
-    (error) => {
-      console.error(error.message);
-      throw error;
-    },
-  )
-  .then(
-    (value) => {
+    })
+  .then((value) => {
       console.log(`Pramusaji memberikan ${value} pesanan.`);
       console.log(`Saya mendapatkan ${value} dan menghabiskannya.`);
-    },
-    (error) => {
-      console.error(error.message);
-      throw error;
-    },
-  );
+    })
+    .catch((error)=>{
+        console.log(error.message);
+    });
